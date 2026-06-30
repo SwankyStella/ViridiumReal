98077d7c666bf12c8886db12fe5e28a14301de46
Eggwaa <willgeisler1413@gmail.com>
2026-06-30 19:20:42 -0400

    ItemBlocks for american sycamore + bald cypress

---
+++ src/main/java/net/junedev/viridium/items/blocks/baldCypressItem.java
@@ -0,0 +1,26 @@
+package net.junedev.viridium.items.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+
+public class baldCypressItem extends ItemBlock {
+
+    private String unlocalizedName;
+
+    public baldCypressItem(Block block) {
+        super(block);
+
+    }
+    @Override
+    public String getUnlocalizedName(ItemStack stack) {
+        return "item." + "bald_cypress_log";
+    }
+
+    @Override
+    public ItemBlock setUnlocalizedName(String name)
+    {
+        this.unlocalizedName = name;
+        return this;
+    }
+}
---
+++ src/main/java/net/junedev/viridium/items/blocks/sycamoreItem.java
@@ -0,0 +1,26 @@
+package net.junedev.viridium.items.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+
+public class sycamoreItem extends ItemBlock {
+
+    private String unlocalizedName;
+
+    public sycamoreItem(Block block) {
+        super(block);
+
+    }
+    @Override
+    public String getUnlocalizedName(ItemStack stack) {
+        return "item." + "american_sycamore_log";
+    }
+
+    @Override
+    public ItemBlock setUnlocalizedName(String name)
+    {
+        this.unlocalizedName = name;
+        return this;
+    }
+}
