9be357c4e406d45ebc821c9d269a2dc1c3e85cb3
Eggwaa <willgeisler1413@gmail.com>
2026-06-30 19:19:53 -0400

    Added generic class for large (normal) logs

---
+++ src/main/java/net/junedev/viridium/blocks/BaseFullLog.java
@@ -0,0 +1,46 @@
+package net.junedev.viridium.blocks;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.block.BlockLog;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.util.IIcon;
+
+public class BaseFullLog extends BlockLog {
+
+    public IIcon[] sides;
+    public IIcon[] tops;
+    public String[] texNames = new String[]{"american_sycamore_log", "bald_cypress_log", "baobab_log"};
+
+    public BaseFullLog() {
+        super();
+        this.setHardness(1.0F); // Hardness of vanilla logs?
+        // this.setCreativeTab TODO: Creative tab
+        this.setStepSound(soundTypeWood);
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerBlockIcons(IIconRegister register) {
+        this.sides = new IIcon[texNames.length];
+        this.tops = new IIcon[texNames.length];
+
+        for (int i = 0; i < texNames.length; ++i) {
+            this.sides[i] = register.registerIcon("viridium:" + texNames[i] + "_side");
+            this.tops[i] = register.registerIcon("viridium:" + texNames[i] + "_top");
+        }
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public IIcon getIcon(int side, int meta)
+    {
+        int k = meta & 12;
+        int l = meta & 3;
+        return k == 0 && (side == 1 || side == 0) ? this.tops[l] : (k == 4 && (side == 5 || side == 4) ? this.tops[l]: (k == 8 && (side == 2 || side == 3) ? this.tops[l] : this.sides[l]));
+    }
+}
+
+
+
+
