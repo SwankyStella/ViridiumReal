767b10b67d79f40ee580a29cb4830f62994be270
Eggwaa <willgeisler1413@gmail.com>
2026-07-01 22:20:24 -0400

    Fixed classes used by each log item type

--- src/main/java/net/junedev/viridium/ViriBlocks.java
+++ src/main/java/net/junedev/viridium/ViriBlocks.java
@@ -1,8 +1,7 @@
 package net.junedev.viridium;
 
 import net.junedev.viridium.blocks.BaseFullLog;
-import net.junedev.viridium.items.blocks.baldCypressLogItem;
-import net.junedev.viridium.items.blocks.sycamoreLogItem;
+import net.junedev.viridium.items.blocks.*;
 import net.minecraft.block.Block;
 
 import cpw.mods.fml.common.registry.GameRegistry;
@@ -30,25 +29,25 @@ public class ViriBlocks {
         GameRegistry.registerBlock(baldCypressLog, baldCypressLogItem.class, "bald_cypress_log");
 
         baobabLog = new BaseFullLog().setBlockName("baobab_log");
-        GameRegistry.registerBlock(baobabLog, baldCypressLogItem.class, "baobab_log");
+        GameRegistry.registerBlock(baobabLog, baobabLogItem.class, "baobab_log");
 
         blackPineLog = new BaseFullLog().setBlockName("black_pine_log");
-        GameRegistry.registerBlock(blackPineLog, baldCypressLogItem.class, "black_pine_log");
+        GameRegistry.registerBlock(blackPineLog, blackPineLogItem.class, "black_pine_log");
 
         dragonsBloodLog = new BaseFullLog().setBlockName("dragons_blood_log");
-        GameRegistry.registerBlock(dragonsBloodLog, baldCypressLogItem.class, "dragons_blood_log");
+        GameRegistry.registerBlock(dragonsBloodLog, dragonsBloodLogItem.class, "dragons_blood_log");
 
         royalPalmLog = new BaseFullLog().setBlockName("royal_palm_log");
-        GameRegistry.registerBlock(royalPalmLog, baldCypressLogItem.class, "royal_palm_log");
+        GameRegistry.registerBlock(royalPalmLog, royalPalmLogItem.class, "royal_palm_log");
 
         sierraJuniperLog = new BaseFullLog().setBlockName("sierra_juniper_log");
-        GameRegistry.registerBlock(sierraJuniperLog, baldCypressLogItem.class, "sierra_juniper_log");
+        GameRegistry.registerBlock(sierraJuniperLog, sierraJuniperLogItem.class, "sierra_juniper_log");
 
         sweetgumLog = new BaseFullLog().setBlockName("sweetgum_log");
-        GameRegistry.registerBlock(sweetgumLog, baldCypressLogItem.class, "sweetgum_log");
+        GameRegistry.registerBlock(sweetgumLog, sweetgumLogItem.class, "sweetgum_log");
 
         weepingWillowLog = new BaseFullLog().setBlockName("weeping_willow_log");
-        GameRegistry.registerBlock(weepingWillowLog, baldCypressLogItem.class, "weeping_willow_log");
+        GameRegistry.registerBlock(weepingWillowLog, weepingWillowLogItem.class, "weeping_willow_log");
     }
 
     // Bushes
